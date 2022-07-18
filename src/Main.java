9efb356f47074a49b7f78f2d8195b00deb51a57d
Анна Лаврентьева <anncom@bk.ru>
2022-07-18 20:45:39 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int checkingAccount = 2000;
-        int depositAmount  = 2000;
+        int depositAmount = 2000;
         int bonus = 100;
-        int balance = depositAmount/bonus;
+        int balance = depositAmount / bonus;
 
-        if (depositAmount > 1000){
+        if (depositAmount > 1000) {
             System.out.println("Ваш баланс составляет " + (checkingAccount + depositAmount + balance) + " рублей!");
         } else {
-            System.out.println("Ваш баланс составляет " + (checkingAccount + depositAmount) +" рублей!");
+            System.out.println("Ваш баланс составляет " + (checkingAccount + depositAmount) + " рублей!");
         }
 
     }
